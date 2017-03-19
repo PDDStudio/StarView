7f0fff27f3826b3894a433f55c3b287d0b590214
tristan <tristan.vanderaerden@gmail.com>
2017-03-19 21:28:00 +0100

    * Add isInEditMode check to onVisibilityChanged method to make StarView able to build in edit mode

--- library/src/main/java/com/pddstudio/starview/StarView.java
+++ library/src/main/java/com/pddstudio/starview/StarView.java
@@ -83,6 +83,11 @@ public class StarView extends GLTextureView {
     @Override
     protected void onVisibilityChanged(@NonNull View changedView, int visibility) {
         super.onVisibilityChanged(changedView, visibility);
+
+        if (isInEditMode()) {
+            return;
+        }
+
         if (visibility == View.VISIBLE) {
             //Pause the starView
             onResume();
